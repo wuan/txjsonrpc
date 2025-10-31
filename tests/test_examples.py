aaca4af926eb0babebe11bb6ef2fb1922fdc47f8
Andreas Würl <andi@tryb.de>
2025-10-31 08:02:05 +0100

    fix

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,4 +1,5 @@
 import os
+import signal
 import sys
 import tempfile
 from subprocess import Popen, PIPE
@@ -48,37 +49,40 @@ Shutting down reactor...
         ("web/client.py", "web/server.tac", result_web,),
         ("webAuth/client.py", "webAuth/server.tac", result_web_auth),
 ))
-def test_example(client, server, expected_result):
+def test_example(client, server, expected_result, tmpdir):
     examples_path = os.path.join(os.path.dirname(__file__), '../examples')
-    with tempfile.NamedTemporaryFile(delete=True) as temp_file:
-        temp_file_name = temp_file.name
 
-        expected_result = preprocess(expected_result)
-        print("Checking examples/%s against examples/%s ..." % (client, server))
-        # start server
-        command = f"twistd -l {temp_file_name} -noy {os.path.join(examples_path, server)}"
-        server_process = Popen(command, shell=True)
-        pid = server_process.pid
-        sleep(0.5)
-        print("server started (pid=%d)" % pid)
-        # run client
-        command = "python %s" % os.path.join(examples_path, client)
-        process = Popen(command, shell=True, stdout=PIPE)
-        (stdout, stderr) = process.communicate()
-        if stderr is not None:
-            print("ERR", stderr)
-        output = stdout.decode("utf-8")
-        result = preprocess(output)
-        print("client finished")
-        # kill server
-        os.kill(pid, 15)
-        # check results
-        if result != expected_result:
-            print("ERROR: expected '%s' but got '%s'" % (expected_result, result))
-            print_result(expected_result)
-            print("but got")
-            print_result(result)
-            print("-- log output:")
-            with open(temp_file_name, 'r') as log_file:
-                print(log_file.read())
-            sys.exit(1)
+    temp_file_name = tmpdir.join("log.txt")
+    pid_file_name = tmpdir.join("pid.txt")
+
+    expected_result = preprocess(expected_result)
+    print("Checking examples/%s against examples/%s ..." % (client, server))
+    # start server
+    command = f"twistd --pidfile {pid_file_name} -l {temp_file_name} -noy {os.path.join(examples_path, server)}"
+    server_process = Popen(command, shell=True)
+    pid = server_process.pid
+    sleep(0.5)
+    print("server started (pid=%d)" % pid)
+    # run client
+    command = "python %s" % os.path.join(examples_path, client)
+    process = Popen(command, shell=True, stdout=PIPE)
+    (stdout, stderr) = process.communicate()
+    if stderr is not None:
+        print("ERR", stderr)
+    output = stdout.decode("utf-8")
+    result = preprocess(output)
+    print("client finished")
+    # kill server
+    os.kill(pid, signal.SIGTERM)
+    sleep(0.5)
+    os.kill(pid, signal.SIGKILL)
+    # check results
+    if result != expected_result:
+        print("ERROR: expected '%s' but got '%s'" % (expected_result, result))
+        print_result(expected_result)
+        print("but got")
+        print_result(result)
+        print("-- log output:")
+        with open(temp_file_name, 'r') as log_file:
+            print(log_file.read())
+        sys.exit(1)
